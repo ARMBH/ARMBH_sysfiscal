d63fade45dac87af899246b86f67b5702a12f141
Daniel Perrout de Castro <m752616@ca.intra>
2019-05-23 16:28:30 -0300

    Altera title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
+import logo1 from './logo.PNG';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo1} className="App-logo" alt="logo" />
+				<p>Sistema de Fiscalização de Parcelamento do Solo para Fins Urbanos</p>
+				<a
+					className="App-link"
+					href="https://github.com/danperrout/ARMBH_sysfiscal/blob/master/README.md"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Visite o repositório
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
