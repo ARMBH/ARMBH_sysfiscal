d37feb86d383f4fe11b3cca39ea1a6741cbedb15
Daniel (Work) <m752616@ca.intra>
2019-05-30 16:58:02 -0300

    Tweak no Auth0 - ainda nao consegue renovar token

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,46 @@ import TodoPrivateWrapper from './Todo/TodoPrivateWrapper';
 import TodoPublicWrapper from './Todo/TodoPublicWrapper';
 import OnlineUsersWrapper from './OnlineUsers/OnlineUsersWrapper';
 
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+
+const createApolloClient = authToken => {
+	return new ApolloClient({
+		link: new HttpLink({
+			uri: 'http://localhost:8080/v1/graphql',
+			headers: {
+				Authorization: `Bearer ${authToken}`
+			}
+		}),
+		cache: new InMemoryCache()
+	});
+};
+
 const App = ({ auth }) => {
+	const client = createApolloClient(auth.idToken);
 	return (
-		<div>
-			<Header logoutHandler={auth.logout} />
-			<div className="container-fluid p-left-right-0">
-				<div className="col-xs-12 col-md-9 p-left-right-0">
-					<div className="col-xs-12 col-md-6 sliderMenu p-30">
-						<TodoPrivateWrapper />
+		<ApolloProvider client={client}>
+			<div>
+				<Header logoutHandler={auth.logout} />
+				<div className="container-fluid p-left-right-0">
+					<div className="col-xs-12 col-md-9 p-left-right-0">
+						<div className="col-xs-12 col-md-6 sliderMenu p-30">
+							<TodoPrivateWrapper />
+						</div>
+						<div className="col-xs-12 col-md-6 sliderMenu p-30 bg-gray border-right">
+							<TodoPublicWrapper />
+						</div>
 					</div>
-					<div className="col-xs-12 col-md-6 sliderMenu p-30 bg-gray border-right">
-						<TodoPublicWrapper />
-					</div>
-				</div>
-				<div className="col-xs-12 col-md-3 p-left-right-0">
-					<div className="col-xs-12 col-md-12 sliderMenu p-30 bg-gray">
-						<OnlineUsersWrapper />
+					<div className="col-xs-12 col-md-3 p-left-right-0">
+						<div className="col-xs-12 col-md-12 sliderMenu p-30 bg-gray">
+							<OnlineUsersWrapper />
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</ApolloProvider>
 	);
 };
 
