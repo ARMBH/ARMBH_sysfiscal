7827962967a4c56129bb5cca4953b7cd2849df5a
Daniel (Work) <m752616@ca.intra>
2019-05-31 12:45:38 -0300

    Ping online user já funciona

--- src/components/OnlineUsers/OnlineUsersWrapper.js
+++ src/components/OnlineUsers/OnlineUsersWrapper.js
@@ -1,40 +1,54 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { withApollo } from 'react-apollo';
 
-import OnlineUser from "./OnlineUser";
+import OnlineUser from './OnlineUser';
 
 class OnlineUsersWrapper extends Component {
-  constructor() {
-    super();
+	constructor(props) {
+		super(props);
+		this.client = props.client;
 
-    this.state = {
-      onlineUsers: [
-        { name: "someUser1" },
-        { name: "someUser2" }
-      ]
-    };
-  }
+		this.state = {
+			onlineUsers: [{ name: 'someUser1' }, { name: 'someUser2' }]
+		};
+	}
 
-  render() {
-    const onlineUsersList = [];
-    this.state.onlineUsers.forEach((user, index) => {
-      onlineUsersList.push(
-        <OnlineUser
-          key={index}
-          index={index}
-          user={user}
-        />);
-    });
+	updateLastSeen() {
+		console.log('Ping last_seen');
+		// Use the apollo client to run a mutation to update the last_seen value
+		const UPDATE_LASTSEEN_MUTATION = gql`
+			mutation updateLastSeen($now: timestamptz!) {
+				update_users(where: {}, _set: { last_seen: $now }) {
+					affected_rows
+				}
+			}
+		`;
+		this.client.mutate({
+			mutation: UPDATE_LASTSEEN_MUTATION,
+			variables: { now: new Date().toISOString() }
+		});
+	}
 
-    return (
-      <div className="onlineUsersWrapper">
-        <div className="sliderHeader">
-          Online users - {this.state.onlineUsers.length}
-        </div>
+	componentDidMount() {
+		// Every 30s, run a mutation to tell the backend that you're online
+		this.onlineIndicator = setInterval(() => this.updateLastSeen(), 30000);
+	}
 
-        { onlineUsersList }
-      </div>
-    );
-  }
+	render() {
+		const onlineUsersList = [];
+		this.state.onlineUsers.forEach((user, index) => {
+			onlineUsersList.push(<OnlineUser key={index} index={index} user={user} />);
+		});
+
+		return (
+			<div className="onlineUsersWrapper">
+				<div className="sliderHeader">Online users - {this.state.onlineUsers.length}</div>
+
+				{onlineUsersList}
+			</div>
+		);
+	}
 }
 
-export default OnlineUsersWrapper;
+export default withApollo(OnlineUsersWrapper);
