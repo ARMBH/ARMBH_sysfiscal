e1a77be08161cd38f2cc0c571638b2ef8f317d59
Daniel (Work) <m752616@ca.intra>
2019-05-31 11:27:03 -0300

    Para remover Warning: optimisticResponse: null

--- src/components/Todo/TodoItem.js
+++ src/components/Todo/TodoItem.js
@@ -1,39 +1,62 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { withApollo } from 'react-apollo';
+import { GET_MY_TODOS } from './TodoPrivateList';
 
-const TodoItem = ({index, todo}) => {
+const TodoItem = ({ index, todo, client }) => {
+	const removeTodo = e => {
+		e.preventDefault();
+		e.stopPropagation();
+	};
 
-  const removeTodo = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
+	const TOGGLE_TODO = gql`
+		mutation toggleTodo($id: Int!, $isCompleted: Boolean!) {
+			update_todos(where: { id: { _eq: $id } }, _set: { is_completed: $isCompleted }) {
+				affected_rows
+			}
+		}
+	`;
 
-  const toggleTodo = () => {};
+	const toggleTodo = () => {
+		client.mutate({
+			mutation: TOGGLE_TODO,
+			variables: { id: todo.id, isCompleted: !todo.is_completed },
+			optimisticResponse: null,
+			update: cache => {
+				const existingTodos = cache.readQuery({ query: GET_MY_TODOS });
+				const newTodos = existingTodos.todos.map(t => {
+					if (t.id === todo.id) {
+						return { ...t, is_completed: !t.is_completed };
+					} else {
+						return t;
+					}
+				});
+				cache.writeQuery({
+					query: GET_MY_TODOS,
+					data: { todos: newTodos }
+				});
+			}
+		});
+	};
 
-  return (
-    <li>
-      <div className="view">
-        <div className="round">
-          <input
-            checked={todo.is_completed}
-            type="checkbox"
-            id={todo.id}
-            onChange={toggleTodo}
-          />
-          <label htmlFor={todo.id}/>
-        </div>
-      </div>
+	return (
+		<li>
+			<div className="view">
+				<div className="round">
+					<input checked={todo.is_completed} type="checkbox" id={todo.id} onChange={toggleTodo} />
+					<label htmlFor={todo.id} />
+				</div>
+			</div>
 
-      <div className={"labelContent" + (todo.is_completed ? " completed" : '')}>
-        <div>
-          {todo.title}
-        </div>
-      </div>
+			<div className={'labelContent' + (todo.is_completed ? ' completed' : '')}>
+				<div>{todo.title}</div>
+			</div>
 
-      <button className="closeBtn" onClick={removeTodo}>
-        x
-      </button>
-    </li>
-  );
+			<button className="closeBtn" onClick={removeTodo}>
+				x
+			</button>
+		</li>
+	);
 };
 
-export default TodoItem;
+export default withApollo(TodoItem);
