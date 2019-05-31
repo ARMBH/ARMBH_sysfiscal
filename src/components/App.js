dbb4a10256a79136b82c730907778ba60f302ea7
Daniel (Work) <m752616@ca.intra>
2019-05-31 14:20:01 -0300

    Online User Ok

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,20 @@ import OnlineUsersWrapper from './OnlineUsers/OnlineUsersWrapper';
 
 import ApolloClient from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
 import { ApolloProvider } from 'react-apollo';
 
 const createApolloClient = authToken => {
 	return new ApolloClient({
-		link: new HttpLink({
-			uri: 'http://localhost:8080/v1/graphql',
-			headers: {
-				Authorization: `Bearer ${authToken}`
+		link: new WebSocketLink({
+			uri: 'ws://localhost:8080/v1/graphql',
+			options: {
+				reconnect: true,
+				connectionParams: {
+					headers: {
+						Authorization: `Bearer ${authToken}`
+					}
+				}
 			}
 		}),
 		cache: new InMemoryCache()
