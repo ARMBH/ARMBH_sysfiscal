7362f555c3737067c4a55b0f61c15a6b4bb40a60
Daniel (Work) <m752616@ca.intra>
2019-06-28 11:45:12 -0300

    Contador em ListaProcessos

--- src/components/Processo/ListaProcessos.js
+++ src/components/Processo/ListaProcessos.js
@@ -24,7 +24,7 @@ class ListaProcessos extends Component {
     let contentTitle = "Lista de todos os processos";
     if (id) contentTitle = "Editar Processo nº " + id;
 
-    let cardTitle = "Mostrando todos os processos";
+    let cardTitle = "Nenhum processo encontrado";
     if (id) cardTitle = title + "";
 
     return (
@@ -46,80 +46,89 @@ class ListaProcessos extends Component {
             </Grid.Col>
           </Grid.Row>
           <Grid.Row cards deck>
-            <Card title={cardTitle}>
-              <Query query={QUERY_PROCESSOS} pollInterval={500}>
-                {({ loading, error, data }) => {
-                  if (loading) return "Carregando...";
-                  if (error) return `Error! ${error.message}`;
-                  return (
-                    <React.Fragment>
-                      <Table
-                        cards={true}
-                        striped={true}
-                        responsive={true}
-                        className="table-vcenter"
-                      >
-                        <Table.Header>
-                          <Table.Row>
-                            <Table.ColHeader>Nº</Table.ColHeader>
-                            <Table.ColHeader>Descrição</Table.ColHeader>
-                            <Table.ColHeader>Criado por</Table.ColHeader>
-                            <Table.ColHeader>Status</Table.ColHeader>
-                            <Table.ColHeader>Criado em</Table.ColHeader>
-                            <Table.ColHeader>Últ. atualização</Table.ColHeader>
-                            <Table.ColHeader />
-                          </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                          {data.processos.map((processo, index) => (
-                            <Table.Row key={index}>
-                              <Table.Col>{processo.id}</Table.Col>
-                              <Table.Col>{processo.title}</Table.Col>
-                              <Table.Col>{processo.user.name}</Table.Col>
-                              <Table.Col>
-                                {Moment().diff(processo.created_at, "hours") <
-                                24 ? (
-                                  <Badge color="success">Novo</Badge>
-                                ) : (
-                                  ""
-                                )}
-                              </Table.Col>
-                              <Table.Col>
-                                <DataPorExtenso data={processo.created_at} />
-                              </Table.Col>
-                              <Table.Col>
-                                <DataPorExtenso data={processo.updated_at} />
-                              </Table.Col>
-                              <Table.Col>
-                                {userLogado === processo.user.id ? (
-                                  <Button
-                                    size="sm"
-                                    color="primary"
-                                    icon="edit"
-                                    onClick={() =>
-                                      this.gotoProcesso(processo.id)
-                                    }
-                                  />
-                                ) : (
-                                  <Button
-                                    size="sm"
-                                    color="secondary"
-                                    icon="eye"
-                                    onClick={() =>
-                                      this.gotoProcesso(processo.id)
-                                    }
-                                  />
-                                )}
-                              </Table.Col>
+            <Query query={QUERY_PROCESSOS} pollInterval={500}>
+              {({ loading, error, data }) => {
+                if (loading) return "Carregando...";
+                if (error) return `Error! ${error.message}`;
+                if (data.processos.length > 0)
+                  cardTitle =
+                    "Mostrando " + data.processos.length + " processos";
+                return (
+                  <React.Fragment>
+                    <Card title={cardTitle}>
+                      {data.processos.length > 0 ? (
+                        <Table
+                          cards={true}
+                          striped={true}
+                          responsive={true}
+                          className="table-vcenter"
+                        >
+                          <Table.Header>
+                            <Table.Row>
+                              <Table.ColHeader>Nº</Table.ColHeader>
+                              <Table.ColHeader>Descrição</Table.ColHeader>
+                              <Table.ColHeader>Criado por</Table.ColHeader>
+                              <Table.ColHeader>Status</Table.ColHeader>
+                              <Table.ColHeader>Criado em</Table.ColHeader>
+                              <Table.ColHeader>
+                                Últ. atualização
+                              </Table.ColHeader>
+                              <Table.ColHeader />
                             </Table.Row>
-                          ))}
-                        </Table.Body>
-                      </Table>
-                    </React.Fragment>
-                  );
-                }}
-              </Query>
-            </Card>
+                          </Table.Header>
+                          <Table.Body>
+                            {data.processos.map((processo, index) => (
+                              <Table.Row key={index}>
+                                <Table.Col>{processo.id}</Table.Col>
+                                <Table.Col>{processo.title}</Table.Col>
+                                <Table.Col>{processo.user.name}</Table.Col>
+                                <Table.Col>
+                                  {Moment().diff(processo.created_at, "hours") <
+                                  24 ? (
+                                    <Badge color="success">Novo</Badge>
+                                  ) : (
+                                    ""
+                                  )}
+                                </Table.Col>
+                                <Table.Col>
+                                  <DataPorExtenso data={processo.created_at} />
+                                </Table.Col>
+                                <Table.Col>
+                                  <DataPorExtenso data={processo.updated_at} />
+                                </Table.Col>
+                                <Table.Col>
+                                  {userLogado === processo.user.id ? (
+                                    <Button
+                                      size="sm"
+                                      color="primary"
+                                      icon="edit"
+                                      onClick={() =>
+                                        this.gotoProcesso(processo.id)
+                                      }
+                                    />
+                                  ) : (
+                                    <Button
+                                      size="sm"
+                                      color="secondary"
+                                      icon="eye"
+                                      onClick={() =>
+                                        this.gotoProcesso(processo.id)
+                                      }
+                                    />
+                                  )}
+                                </Table.Col>
+                              </Table.Row>
+                            ))}
+                          </Table.Body>
+                        </Table>
+                      ) : (
+                        <Card.Body>Nenhum processo encontrado.</Card.Body>
+                      )}
+                    </Card>
+                  </React.Fragment>
+                );
+              }}
+            </Query>
           </Grid.Row>
         </Page.Content>
       </SiteWrapper>
