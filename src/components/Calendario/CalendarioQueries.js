5d01d199cb4c7276fcee4d1125edf9316bb2bc1c
Daniel (Work) <m752616@ca.intra>
2019-08-01 11:21:06 -0300

    Remove "Abertura de Processo" do calendario

--- src/components/Calendario/CalendarioQueries.js
+++ src/components/Calendario/CalendarioQueries.js
@@ -3,7 +3,10 @@ import gql from "graphql-tag";
 const QUERY_PROCESSOS_STATUS = gql`
   query getProcesso($startDate: timestamptz, $endDate: timestamptz) {
     processos_status(
-      where: { due_date: { _gte: $startDate, _lte: $endDate } }
+      where: {
+        due_date: { _gte: $startDate, _lte: $endDate }
+        _not: { status_id: { _eq: 1 } }
+      }
     ) {
       created_at
       due_date
