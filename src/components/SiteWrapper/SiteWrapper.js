ff4923b8407bb73180cb0c57bc6abcd063d9d154
Daniel (Work) <m752616@ca.intra>
2019-08-14 11:43:00 -0300

    Static build presente

--- src/components/SiteWrapper/SiteWrapper.js
+++ src/components/SiteWrapper/SiteWrapper.js
@@ -336,7 +336,7 @@ class SiteWrapper extends React.Component<Props, State> {
                 </span>
               )}
               <br />
-              Versão: 1.0.1 Alpha
+              Versão: 1.0.2 Alpha
             </React.Fragment>
           ),
           copyright: (
